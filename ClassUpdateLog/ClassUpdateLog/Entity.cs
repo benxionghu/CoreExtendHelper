f0f1abc945cfc1a625406a1b926b8044ba4ecaf3
胡本雄 <1301536601@qq.com>
2020-02-09 10:21:44 +0800

    新增特性

---
+++ ClassUpdateLog/ClassUpdateLog/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassUpdateLog
+{
+    [PropertyChangeTracking]
+    public class Entity
+    {
+        [PropertyChangeTracking(ignore: true)]
+        public Guid Id { get; set; }
+
+        [PropertyChangeTracking(displayName: "序号")]
+        public string OId { get; set; }
+
+        [PropertyChangeTracking(displayName: "第一个字段")]
+        public string A { get; set; }
+
+        public double B { get; set; }
+
+        public bool C { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
---
+++ ClassUpdateLog/ClassUpdateLog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassUpdateLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstEntity = new Entity()
+            {
+
+                Id = Guid.NewGuid(),
+                OId = DateTime.Now.ToString("yyyyMMddHHmmss"),
+                A = "a1",
+                B = 0.01,
+                C = true,
+            };
+            var lastEntity = new Entity()
+            {
+                A = "a2",
+                B = 0.02,
+                C = true
+            };
+            //记录修改的字段
+            var logs = firstEntity.GetPropertyLogs(lastEntity);
+            Console.ReadKey();
+        }
+    }
+}
