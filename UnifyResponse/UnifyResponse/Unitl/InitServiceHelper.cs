1b20604eecb2281c243e544b36dd29e97b8abc1c
hubenxiong <hubenxiong@niuguwang.com>
2020-05-26 10:59:40 +0800

    新增批量注入方式

---
+++ UnifyResponse/UnifyResponse/Unitl/InitServiceHelper.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnifyResponse.Unitl
+{
+    /// <summary>
+    /// 初始化服务
+    /// </summary>
+    public static class InitServiceHelper
+    {
+        /// <summary>
+        /// Add Scoped from InterfaceAssembly and ImplementAssembly to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="interfaceAssembly"></param>
+        /// <param name="implementAssembly"></param>
+        public static void AddScoped(this IServiceCollection services, Assembly interfaceAssembly, Assembly implementAssembly)
+        {
+            var interfaces = interfaceAssembly.GetTypes().Where(t => t.IsInterface);
+            var implements = implementAssembly.GetTypes();
+            foreach (var item in interfaces)
+            {
+                var type = implements.FirstOrDefault(x => item.IsAssignableFrom(x));
+                if (type != null)
+                {
+                    services.AddScoped(item, type);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add AddSingleton from InterfaceAssembly and ImplementAssembly to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="interfaceAssembly"></param>
+        /// <param name="implementAssembly"></param>
+        public static void AddSingleton(this IServiceCollection services, Assembly interfaceAssembly, Assembly implementAssembly)
+        {
+            var interfaces = interfaceAssembly.GetTypes().Where(t => t.IsInterface);
+            var implements = implementAssembly.GetTypes();
+            foreach (var item in interfaces)
+            {
+                var type = implements.FirstOrDefault(x => item.IsAssignableFrom(x));
+                if (type != null)
+                {
+                    services.AddSingleton(item, type);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add AddTransient from InterfaceAssembly and ImplementAssembly to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="interfaceAssembly"></param>
+        /// <param name="implementAssembly"></param>
+        public static void AddTransient(this IServiceCollection services, Assembly interfaceAssembly, Assembly implementAssembly)
+        {
+            var interfaces = interfaceAssembly.GetTypes().Where(t => t.IsInterface);
+            var implements = implementAssembly.GetTypes();
+            foreach (var item in interfaces)
+            {
+                var type = implements.FirstOrDefault(x => item.IsAssignableFrom(x));
+                if (type != null)
+                {
+                    services.AddTransient(item, type);
+                }
+            }
+        }
+    }
+}
